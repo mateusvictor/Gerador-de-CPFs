e8017c52d368d205ed310ec051d8b41b81f06755
unknown <mateus_victor_@outlook.com>
2021-02-15 19:49:49 -0300

    Updated settings.py

--- gerador_de_cpfs/settings.py
+++ gerador_de_cpfs/settings.py
@@ -30,7 +30,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['gerar-cpfs.herokuapp.com']
 #ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -51,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'gerador_de_cpfs.urls'
