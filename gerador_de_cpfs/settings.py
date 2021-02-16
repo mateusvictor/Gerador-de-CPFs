9fdc2e1ea2ae7f61af440c1e525d54014459e09b
unknown <mateus_victor_@outlook.com>
2021-02-16 17:38:51 -0300

    Updated settings.py

--- gerador_de_cpfs/settings.py
+++ gerador_de_cpfs/settings.py
@@ -22,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zywd11)u5v(zi7#%#98wv2za(hetyglyju@!#_d3o8x-km(=90'
-#SECRET_KEY = os.environ.get('SECRET_KEY_CPF')
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['gerar-cpfs.herokuapp.com']
 #ALLOWED_HOSTS = []
